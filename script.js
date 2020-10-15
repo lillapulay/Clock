7572c494eb6ed7ad3caffa87da1518261cc13534
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-10-15 11:56:04 +0200

    Logs the second every 1 sec

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function setDate() {
-  console.log('Hi');
+  const now = new Date();
+  const second = now.getSeconds();
+  console.log(second);
 }
 
 setInterval(setDate, 1000); // Executes setDate fn every 1 sec
